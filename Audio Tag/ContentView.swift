7105cafd0c6f85dbb4cbd5a682651625a5b92844
topfloordaws <dawsonpham@gmail.com>
2025-06-24 21:43:34 -0700

    Initial Commit

---
+++ Audio Tag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Audio Tag
+//
+//  Created by Dawson Pham on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Audio TagTests/Audio_TagTests.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_TagTests.swift
+//  Audio TagTests
+//
+//  Created by Dawson Pham on 6/24/25.
+//
+
+import Testing
+@testable import Audio_Tag
+
+struct Audio_TagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
